d6d5b68a9edd99f79106b7867b32eb8eaa87be44
sora-uzu <naruto07170717@gmail.com>
2020-08-18 19:18:04 +0900

    [Start]Chapter5

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Requests\HelloRequest;
+use Validator;
+use Illuminate\Support\Facades\DB;
+
+class HelloController extends Controller
+{
+
+    public function index(Request $request) 
+    {
+       $items = DB::select('select * from people');
+        return view('hello.index', ['items'=> $items]);
+    }
+
+    public function post(HelloRequest $request)
+    {
+        $validate_rule = [
+            'msg' => 'required',
+        ];
+        $this->validate($request, $validate_rule);
+        $msg = $request->msg;
+        $response = response()->view('hello.index',['msg'=>'「' . $msg .'」をクッキーに保存しました。']);
+        $response->cookie('msg', $msg, 100);
+     return $response;
+    }
+}
---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Index')
+
+@section('menubar')
+  @parent
+  インデックスページ
+@endsection
+
+@section('content')
+ <table>
+   <tr><th>Name</th><th>
+@endsection
+
+@section('footer')
+copyrght 2020 terui.
+@endsection
