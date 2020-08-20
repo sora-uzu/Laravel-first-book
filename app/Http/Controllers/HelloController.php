7147ed523cda77c743caf9720d08097a62cbef30
sora-uzu <naruto07170717@gmail.com>
2020-08-20 18:41:52 +0900

    [UPDATE]Chapter5

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -13,19 +13,69 @@ class HelloController extends Controller
 
     public function index(Request $request) 
     {
-       $items = DB::select('select * from people');
+        $items = DB::table('people2')->get();
         return view('hello.index', ['items'=> $items]);
     }
 
-    public function post(HelloRequest $request)
+    public function post(Request $request)
     {
-        $validate_rule = [
-            'msg' => 'required',
+        $items = DB::select('select * from people2');
+        return view('hello.index', ['items'=> $items]);   
+    }
+
+    public function add(Request $request)
+    {
+        return view('hello.add');   
+    }
+
+    public function create(Request $request)
+    {
+        $param = [
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
         ];
-        $this->validate($request, $validate_rule);
-        $msg = $request->msg;
-        $response = response()->view('hello.index',['msg'=>'「' . $msg .'」をクッキーに保存しました。']);
-        $response->cookie('msg', $msg, 100);
-     return $response;
+        DB::insert('insert into people2 (name, mail, age) values(:name, :mail, :age)', $param);
+        return redirect('/hello');
+    }
+
+    public function edit(Request $request)
+    {
+        $param =['id' => $request->id];
+        $item = DB::select('select * from people2 where id = :id', $param);
+        return view('hello.edit', ['form' => $item[0]]);
+    }
+
+    public function update(Request $request)
+    {
+        $param = [
+            'id' => $request->id,
+            'name' => $request->name,
+            'mail' => $request->mail,
+            'age' => $request->age,
+        ];
+        DB::update('update people2 set name = :name, mail = :mail, age = :age where id = :id', $param);
+        return redirect('/hello');
+    }
+
+    public function del(Request $request)
+    {
+        $param =['id' => $request->id];
+        $item = DB::select('select * from people2 where id = :id', $param);
+        return view('hello.del', ['form' => $item[0]]);
+    }
+
+    public function remove(Request $request)
+    {
+        $param =['id' => $request->id];
+        DB::delete('delete from people2 where id = :id', $param);
+        return redirect('/hello');
+    }
+
+    public function show(Request $request) 
+    {
+        $id = $request->id;
+        $item = DB::table('people2')->where('id', $id)->first();
+        return view('hello.show', ['item' => $item]);
     }
 }
--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -9,7 +9,15 @@
 
 @section('content')
  <table>
-   <tr><th>Name</th><th>
+   <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+   @foreach ($items as $item)
+   <tr>
+      <td>{{$item->name}}</td>
+      <td>{{$item->mail}}</td>
+      <td>{{$item->age}}</td>
+   </tr>
+   @endforeach
+ </table>
 @endsection
 
 @section('footer')
